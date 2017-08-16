7b90fe8dd5ed6c1140ff45cf553f22433571cf2f
manu <manunraj123@gmail.com>
2017-08-16 16:18:18 +0530

    Add project files.

---
+++ MedMosey/ShowResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedMosey
+{
+    class ShowResult
+    {
+        string conStr, sheetName;
+        string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+
+        public static string GiveResult()
+        {
+            
+        }
+
+        
+
+
+    }
+}
